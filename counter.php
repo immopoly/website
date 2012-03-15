ff917dc8c91d60c5f0353990e4a93e42961fb246
Yan 'YncYrYdYbYl' Minagawa <t@c-base.org>
2012-03-15 18:13:41 +0100

    counter pre formatted

---
+++ counter.php
@@ -0,0 +1,38 @@
+<?
+function counter() {
+    // {"Counter":{"badgeOneOfTheFirst":441,"date":1331821613933,"exposesRented":238,"user":667}}
+    $url = "http://immopoly.appspot.com/user/counter"; 
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_BINARYTRANSFER, true);
+    $output = curl_exec($ch);
+    curl_close($ch);
+    $foo = json_decode($output,true);
+    print_r($foo["Counter"]["badgeOneOfTheFirst"]);
+}
+?>
+<html><head>
+<style type="text/css">
+     body, pre {
+          margin: 0;
+          padding: 0;
+          border: 0;
+          outline: 0;
+          font-size: 100%;
+          vertical-align: baseline;
+     }
+     body {
+          width: 75px;
+     }
+     pre {
+          font: 1.75em/1 Tahoma, Geneva, Kalimati, sans-serif;
+          color: #444;
+     }
+
+</style>
+</head>
+<body>
+<pre>
+<? counter(); ?>
+</pre>
+</body>
